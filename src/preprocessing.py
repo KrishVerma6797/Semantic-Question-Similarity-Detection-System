eaf0584437b259af12ffca74d6962a47c9e43093
Krish Verma <krishverma6797@gmail.com>
2026-03-12 12:42:10 +0530

    Refactor load_data function for clarity and structure

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,14 +2,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def load_data(path):
-    data=pd.read_csv(path)
-    data=data.dropna()
 
-    q1=data['question1']
-    q2=data['question2']
-    labels=data['is_duplicate']
+    data = pd.read_csv(path)
+    data = data.dropna()
 
-    combined=q1 + "[SEP]" + q2
+    q1 = data["question1"]
+    q2 = data["question2"]
+    labels = data["is_duplicate"]
 
-    return  train_test_split(combined,labels,q1,q2,test_size=0.2,random_state=42)
-    
+    X_train_q1, X_test_q1, X_train_q2, X_test_q2, y_train, y_test = train_test_split(
+        q1, q2, labels, test_size=0.2, random_state=42
+    )
+
+    return X_train_q1, X_test_q1, X_train_q2, X_test_q2, y_train, y_test
