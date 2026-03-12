17f96b58db901cf29a0ed6856b569338e7cd3669
Krish Verma <krishverma6797@gmail.com>
2026-03-12 12:13:10 +0530

    Fix data loading by using correct path variable

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def load_data(path):
-    data=pd.read_csv(data)
+    data=pd.read_csv(path)
     data=data.dropna()
 
     q1=data['question1']
